a8153f93d5a42acfcc2e94f92c380989c8a21fd2
bhargavi-netizen <kotavijayabhargavi@gmail.com>
2026-01-25 17:50:07 +0530

    Update login_navigation_test.py

--- login_navigation_test.py
+++ login_navigation_test.py
@@ -3,34 +3,17 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import options
 from selenium.webdriver.common.by import By
 import time
-
-# Open Firefox browser
 driver = webdriver.Firefox()
-
-# Open sample web application
 driver.get("https://the-internet.herokuapp.com/login")
 driver.maximize_window()
 time.sleep(2)
-
-# LOGIN FUNCTIONALITY
 driver.find_element(By.ID, "username").send_keys("tomsmith")
 driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
 driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 time.sleep(3)
-
-# Verify login
-print("Login Message:",
-      driver.find_element(By.ID, "flash").text)
-
-# NAVIGATION FUNCTIONALITY (Logout)
-driver.find_element(By.CSS_SELECTOR,
-                    "a.button.secondary.radius").click()
+print("Login Message:",driver.find_element(By.ID, "flash").text)
+driver.find_element(By.CSS_SELECTOR,"a.button.secondary.radius").click()
 time.sleep(2)
-
-# Verify navigation
 print("Logout Message:",
       driver.find_element(By.ID, "flash").text)
-
-# Close browser
-
 driver.quit()
